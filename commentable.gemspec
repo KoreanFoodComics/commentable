ea043ac81bb76738d72ecbf0350878193fc17895
Brian Cardarella <bcardarella@gmail.com>
2012-02-18 19:57:02 -0500

    Setup test environment

--- commentable.gemspec
+++ commentable.gemspec
@@ -22,5 +22,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'capybara'
   s.add_development_dependency 'capybara-webkit'
   s.add_development_dependency 'factory_girl_rails'
+  s.add_development_dependency 'bourne'
   s.add_development_dependency 'pry'
 end
--- lib/commentable/engine.rb
+++ lib/commentable/engine.rb
@@ -1,5 +1,21 @@
 module Commentable
   class Engine < ::Rails::Engine
     isolate_namespace Commentable
+
+    config.generators do |g|
+      g.test_framework :rspec, :view_specs => false
+    end
+
+    def self.app_path
+      File.expand_path('../../app', called_from)
+    end
+
+    %w{controller helper mailer model}.each do |resource|
+      class_eval <<-RUBY
+        def self.#{resource}_path(name)
+          File.expand_path("#{resource.pluralize}/\#{parent.name.underscore}/\#{name}.rb", app_path)
+        end
+      RUBY
+    end
   end
 end
